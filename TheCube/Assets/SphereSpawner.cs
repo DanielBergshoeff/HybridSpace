6ce4aa4133a94ff8dd6f435cff88750bc4c48e0c
DanielBergshoeff <dabergshoeff@gmail.com>
2018-10-19 18:40:57 +0200

    Added sphere spawner in cube

---
+++ TheCube/Assets/SphereSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour {
+
+    public GameObject spherePrefab;
+
+    public float timePerSpawn;
+    public float forceUpward;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= timePerSpawn) {
+            GameObject go = Instantiate(spherePrefab, transform.position, Quaternion.identity);
+            go.GetComponent<Rigidbody>().AddForce(new Vector3(0, forceUpward, 0));
+            timer = 0;
+        }
+	}
+}
