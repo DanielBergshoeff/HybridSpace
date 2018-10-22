63aa3008d2c612ee182d35dbae6ca468fdb0ea50
DanielBergshoeff <dabergshoeff@gmail.com>
2018-10-22 13:06:15 +0200

    Added parabolas for orb movement

---
+++ TheCube/Assets/Magnet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MagnetType
+{
+    RED,
+    GREEN,
+    BLUE
+}
+
+public class Magnet : MonoBehaviour {
+
+    public MagnetType myType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
